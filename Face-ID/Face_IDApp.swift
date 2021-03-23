6e855c3a4bea7a8d749fbb24c26a08a8bcac6a2b
RajMalHotRA123 <RM_frankfurt@gmx.de>
2021-03-23 15:32:37 +0100

    Initial Commit

---
+++ Face-ID/Face_IDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Face_IDApp.swift
+//  Face-ID
+//
+//  Created by Raj Malhotra on 23.03.21.
+//
+
+import SwiftUI
+
+@main
+struct Face_IDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
