1a33d8229d5b717ee14cbc50488d6c4e8306ae34
RajMalHotRA123 <RM_frankfurt@gmx.de>
2021-03-24 22:59:25 +0100

    Face-& Touch-ID included

--- Face-ID/ContentView.swift
+++ Face-ID/ContentView.swift
@@ -6,11 +6,47 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct ContentView: View {
+    @State private var isUnlocked = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            if self.isUnlocked {
+                Text("Unlocked")
+            }
+            else{
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        //check whether the biometric authentication is possible#
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            //it's possible, so go ahead and use it
+            let reason = "We need to unlock your data"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason){ success, authenticationError in
+                //authentication has now completed
+                DispatchQueue.main.async {
+                    if success {
+                        //authenticated successfully
+                        self.isUnlocked = true
+                    }
+                    else{
+                        //there was a problem
+                    }
+                }
+            }
+        }
+        else{
+            // no biometrics
+        }
     }
 }
 
